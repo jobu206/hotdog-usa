eb0db099a04b788fb0210b3514221855308fc25b
KevinL <mastahkevin@gmail.com>
2019-09-01 23:53:51 -0700

    Initial build.

---
+++ build/precache-manifest.80ed80a133ed2cf49042bb51ff451f77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521b7b7ca633f0d3d2f56b0f17c4659a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c6bb995d1c58f32298a",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "cf7be1fc648a725614bb",
+    "url": "/static/js/2.56adab47.chunk.js"
+  },
+  {
+    "revision": "5c6bb995d1c58f32298a",
+    "url": "/static/js/main.130f3509.chunk.js"
+  },
+  {
+    "revision": "5cae883e66c56525693c",
+    "url": "/static/js/runtime~main.c883f07b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
