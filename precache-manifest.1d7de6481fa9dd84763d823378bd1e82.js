260b2e31a11af180c2dd3e64ecb4e50c2c3e2b11
KevinL <mastahkevin@gmail.com>
2019-09-02 00:15:00 -0700

    Updates

---
+++ precache-manifest.1d7de6481fa9dd84763d823378bd1e82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbeaff8bc27c89fc91b32eeef78d942b",
+    "url": "/hotdog-usa/index.html"
+  },
+  {
+    "revision": "65fc95927f1ff6562911",
+    "url": "/hotdog-usa/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "cf7be1fc648a725614bb",
+    "url": "/hotdog-usa/static/js/2.56adab47.chunk.js"
+  },
+  {
+    "revision": "65fc95927f1ff6562911",
+    "url": "/hotdog-usa/static/js/main.1faa1000.chunk.js"
+  },
+  {
+    "revision": "ff60256ed90c5a00558c",
+    "url": "/hotdog-usa/static/js/runtime~main.4f4be737.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hotdog-usa/static/media/logo.5d5d9eef.svg"
+  }
+]);
