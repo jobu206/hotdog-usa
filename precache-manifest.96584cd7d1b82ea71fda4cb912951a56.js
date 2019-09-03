2ae68a325fc2e00a269b3d3b0557a8f717bef963
Kevin LeRoy (HCL America Inc) <mastahkevin@gmail.com>
2019-09-03 15:36:30 -0700

    Updates

--- precache-manifest.96584cd7d1b82ea71fda4cb912951a56.js
+++ precache-manifest.96584cd7d1b82ea71fda4cb912951a56.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cb1cc50edf7a6bc0f6b88d3cd35a5f4a",
+    "revision": "71004d4ac6e9faa5a8bf4855e00a3ce4",
     "url": "/hotdog-usa/index.html"
   },
   {
-    "revision": "aee9a709e993d50f7123",
-    "url": "/hotdog-usa/static/css/main.18cba638.chunk.css"
+    "revision": "3ea973f3e6c92d5ba37d",
+    "url": "/hotdog-usa/static/css/main.3014b9fb.chunk.css"
   },
   {
     "revision": "65cc6948aef892abc014",
     "url": "/hotdog-usa/static/js/2.4cd8a365.chunk.js"
   },
   {
-    "revision": "aee9a709e993d50f7123",
-    "url": "/hotdog-usa/static/js/main.704ebdf8.chunk.js"
+    "revision": "3ea973f3e6c92d5ba37d",
+    "url": "/hotdog-usa/static/js/main.2136a7bd.chunk.js"
   },
   {
     "revision": "ff60256ed90c5a00558c",
